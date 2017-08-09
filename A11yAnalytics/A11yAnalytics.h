6ec298cab2ce70a6f36aa06a1a7064180bfd8243
Bryan Clark <contact@bryanjclark.com>
2017-08-09 12:02:30 -0700

    Initial commit

---
+++ A11yAnalytics/A11yAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  A11yAnalytics.h
+//  A11yAnalytics
+//
+//  Created by Bryan Clark on 8/9/17.
+//  Copyright © 2017 Khan Academy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for A11yAnalytics.
+FOUNDATION_EXPORT double A11yAnalyticsVersionNumber;
+
+//! Project version string for A11yAnalytics.
+FOUNDATION_EXPORT const unsigned char A11yAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <A11yAnalytics/PublicHeader.h>
+
+
